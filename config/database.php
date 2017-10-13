a7418c1718814c3d960e3f2c91b25fbd7b27ad9e
Luis Iván García Luna <luis.garcialuna@outlook.com>
2017-10-13 11:18:30 -0500

    asdasd

--- config/database.php
+++ config/database.php
@@ -44,7 +44,7 @@ return [
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'rhino'),
-            'username' => env('DB_USERNAME', 'root'),
+            'username' => env('DB_USERNAME', 'rhino'),
             'password' => env('DB_PASSWORD', 'rhino'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
